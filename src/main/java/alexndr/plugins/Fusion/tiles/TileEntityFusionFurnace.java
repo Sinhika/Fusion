f13d10e86d574b45f2f73484f39cd2e641f43ddd
Sinhika <sinhika@republicofnewhome.org>
2016-11-19 15:58:57 -0600

    gave fusion furnace slot indexes meaningful names; fixed automation issues with slots

--- src/main/java/alexndr/plugins/Fusion/tiles/TileEntityFusionFurnace.java
+++ src/main/java/alexndr/plugins/Fusion/tiles/TileEntityFusionFurnace.java
@@ -14,23 +14,31 @@ import alexndr.plugins.Fusion.blocks.BlockFusionFurnace;
  */
 public class TileEntityFusionFurnace extends TileEntitySimpleFurnace
 {
-	protected static boolean initDone = false;
+	protected static final int NDX_LEFT_SLOT = 0;
+	protected static final int NDX_FUEL_SLOT = 1;
+	protected static final int NDX_OUTPUT_SLOT = 2;
+    protected static final int NDX_RIGHT_SLOT = 3;
+    protected static final int NDX_CATALYST_SLOT = 4;
+    
+    protected static boolean initDone = false;
 	
 //	private static final int[] slots_catalyst = new int[] {4};
 //	private static final int[] slots_output = new int[] {2, 1};
 //	private static final int[] slots_fuel = new int[] {1};
 //	private ItemStack[] furnaceItemStacks = new ItemStack[5];
 	
-	protected static final int[] slots_input1 = new int[] {0};
-	protected static final int[] slots_input2 = new int[] {3};
+	protected static final int[] slots_input1 = new int[] {NDX_LEFT_SLOT};
+	protected static final int[] slots_input2 = new int[] {NDX_RIGHT_SLOT};
+	protected static final int[] slots_fuel = new int[] {NDX_FUEL_SLOT};
+	protected static final int[] slots_output = new int [] {NDX_OUTPUT_SLOT};
 	
 	public static void initThis()
 	{
 		if (initDone) return;
 		
-		slotsTop = new int[] {4}; 		// slots_catalyst
-		slotsBottom = new int[] {2, 1}; // slots_output, slots_fuel
-		slotsSides = new int[] {0, 3};  // slots_input1, slots_input2
+		slotsTop = new int[] {NDX_CATALYST_SLOT}; 		// slots_catalyst
+		slotsBottom = new int[] {NDX_OUTPUT_SLOT, NDX_FUEL_SLOT}; // slots_output, slots_fuel
+		// slotsSides = new int[] {0, 3};  // slots_input1, slots_input2
 		
 		initDone = true;
 	}
@@ -55,7 +63,7 @@ public class TileEntityFusionFurnace extends TileEntitySimpleFurnace
         }
         
         // Fusion Furnace has 3 (!) input stacks to account for.
-        if ((index == 0 || index == 3 || index == 4) && !flag)
+        if ((index == NDX_LEFT_SLOT || index == NDX_RIGHT_SLOT || index == NDX_CATALYST_SLOT) && !flag)
         {
             this.totalCookTime = this.getCookTime(stack);
             this.cookTime = 0;
@@ -76,22 +84,27 @@ public class TileEntityFusionFurnace extends TileEntitySimpleFurnace
 
 		if(!this.worldObj.isRemote)
 		{
-            if (this.isBurning() || this.furnaceItemStacks[1] != null && this.furnaceItemStacks[0] != null
-            	 && this.furnaceItemStacks[3] != null && this.furnaceItemStacks[4] != null )
+            if (this.isBurning() || this.furnaceItemStacks[NDX_FUEL_SLOT] != null 
+                            && this.furnaceItemStacks[NDX_LEFT_SLOT] != null
+            	 && this.furnaceItemStacks[NDX_RIGHT_SLOT] != null 
+            	 && this.furnaceItemStacks[NDX_CATALYST_SLOT] != null )
             {
                 if (!this.isBurning() && this.canSmelt())
                 {
-                    this.currentItemBurnTime = this.furnaceBurnTime = getItemBurnTime(this.furnaceItemStacks[1]);
+                    this.currentItemBurnTime = this.furnaceBurnTime = 
+                                    getItemBurnTime(this.furnaceItemStacks[NDX_FUEL_SLOT]);
 
                     if (this.isBurning())
                     {
                         flag1 = true;
-                        if (this.furnaceItemStacks[1] != null)
+                        if (this.furnaceItemStacks[NDX_FUEL_SLOT] != null)
                         {
-                            --this.furnaceItemStacks[1].stackSize;
-                            if (this.furnaceItemStacks[1].stackSize == 0)
+                            --this.furnaceItemStacks[NDX_FUEL_SLOT].stackSize;
+                            if (this.furnaceItemStacks[NDX_FUEL_SLOT].stackSize == 0)
                             {
-                                this.furnaceItemStacks[1] = furnaceItemStacks[1].getItem().getContainerItem(furnaceItemStacks[1]);
+                                this.furnaceItemStacks[NDX_FUEL_SLOT] = 
+                                    furnaceItemStacks[NDX_FUEL_SLOT].getItem().getContainerItem(
+                                                    furnaceItemStacks[NDX_FUEL_SLOT]);
                             }
                         } // end-if
                     } // end-if
@@ -104,7 +117,7 @@ public class TileEntityFusionFurnace extends TileEntitySimpleFurnace
                     if (this.cookTime == this.totalCookTime)
                     {
                         this.cookTime = 0;
-                        this.totalCookTime = this.getCookTime(this.furnaceItemStacks[0]);
+                        this.totalCookTime = this.getCookTime(this.furnaceItemStacks[NDX_LEFT_SLOT]);
                         this.smeltItem();
                         flag1 = true;
                     }
@@ -137,15 +150,19 @@ public class TileEntityFusionFurnace extends TileEntitySimpleFurnace
 	@Override
 	protected boolean canSmelt()
 	{
-		if(this.furnaceItemStacks[0] != null && this.furnaceItemStacks[3] != null && this.furnaceItemStacks[4] != null)
+		if(this.furnaceItemStacks[NDX_LEFT_SLOT] != null && this.furnaceItemStacks[NDX_RIGHT_SLOT] != null 
+		                && this.furnaceItemStacks[NDX_CATALYST_SLOT] != null)
 		{
-			ItemStack itemstack = FusionFurnaceRecipes.getSmeltingResult(this.furnaceItemStacks[0], 
-														this.furnaceItemStacks[3], this.furnaceItemStacks[4]);
+			ItemStack itemstack = FusionFurnaceRecipes.getSmeltingResult(
+			                                            this.furnaceItemStacks[NDX_LEFT_SLOT], 
+														this.furnaceItemStacks[NDX_RIGHT_SLOT], 
+														this.furnaceItemStacks[NDX_CATALYST_SLOT]);
 			if(itemstack == null) return false;
-			if(this.furnaceItemStacks[2] == null) return true;
-			if(!this.furnaceItemStacks[2].isItemEqual(itemstack)) return false;
-			int result = furnaceItemStacks[2].stackSize + itemstack.stackSize;
-			return (result <= getInventoryStackLimit() && result <= this.furnaceItemStacks[2].getMaxStackSize());
+			if(this.furnaceItemStacks[NDX_OUTPUT_SLOT] == null) return true;
+			if(!this.furnaceItemStacks[NDX_OUTPUT_SLOT].isItemEqual(itemstack)) return false;
+			int result = furnaceItemStacks[NDX_OUTPUT_SLOT].stackSize + itemstack.stackSize;
+			return (result <= getInventoryStackLimit() 
+			                && result <= this.furnaceItemStacks[NDX_OUTPUT_SLOT].getMaxStackSize());
 		}
 		return false;
 	} // end canSmelt()
@@ -155,26 +172,27 @@ public class TileEntityFusionFurnace extends TileEntitySimpleFurnace
 	{
 		if(this.canSmelt())
 		{
-			ItemStack itemstack = FusionFurnaceRecipes.applyFusion(this.furnaceItemStacks[0], 
-																this.furnaceItemStacks[3], this.furnaceItemStacks[4]);
+			ItemStack itemstack = FusionFurnaceRecipes.applyFusion(this.furnaceItemStacks[NDX_LEFT_SLOT], 
+																this.furnaceItemStacks[NDX_RIGHT_SLOT], 
+																this.furnaceItemStacks[NDX_CATALYST_SLOT]);
 			
-			if(this.furnaceItemStacks[2] == null) {
-				this.furnaceItemStacks[2] = itemstack.copy();
+			if(this.furnaceItemStacks[NDX_OUTPUT_SLOT] == null) {
+				this.furnaceItemStacks[NDX_OUTPUT_SLOT] = itemstack.copy();
 			}
-			else if(this.furnaceItemStacks[2].getItem() == itemstack.getItem()) 
+			else if(this.furnaceItemStacks[NDX_OUTPUT_SLOT].getItem() == itemstack.getItem()) 
 			{
-				furnaceItemStacks[2].stackSize += itemstack.stackSize;
+				furnaceItemStacks[NDX_OUTPUT_SLOT].stackSize += itemstack.stackSize;
 			}
 			
 			// Check for empty input slots 0, 3, 4
-            if (this.furnaceItemStacks[0].stackSize <= 0) {
-				furnaceItemStacks[0] = null;
+            if (this.furnaceItemStacks[NDX_LEFT_SLOT].stackSize <= 0) {
+				furnaceItemStacks[NDX_LEFT_SLOT] = null;
             }
-            if (this.furnaceItemStacks[3].stackSize <= 0) {
-				furnaceItemStacks[3] = null;
+            if (this.furnaceItemStacks[NDX_RIGHT_SLOT].stackSize <= 0) {
+				furnaceItemStacks[NDX_RIGHT_SLOT] = null;
             }
-            if (this.furnaceItemStacks[4].stackSize <= 0) {
-				furnaceItemStacks[4] = null;
+            if (this.furnaceItemStacks[NDX_CATALYST_SLOT].stackSize <= 0) {
+				furnaceItemStacks[NDX_CATALYST_SLOT] = null;
             }
 		} // end-if canSmelt
 	} // end smeltItem()
@@ -182,7 +200,7 @@ public class TileEntityFusionFurnace extends TileEntitySimpleFurnace
 	
 	/**
 	 * Gets the burn time from an itemstack.
-	 * @param par1ItemStack The itemstack to get the burn time for.
+	 * @param burnItem The itemstack to get the burn time for.
 	 * @return The burn time, in ticks, for the itemstack. Returns 0 if it's not a fuel source.
 	 */
 	public static int getItemBurnTime(ItemStack burnItem)
@@ -209,9 +227,10 @@ public class TileEntityFusionFurnace extends TileEntitySimpleFurnace
 					return slots_input2;
 				case SOUTH:
 					return slots_input1;
-				case WEST:
 				case EAST:
-					return slotsSides;
+                    return slots_fuel;
+				case WEST:
+				    return slots_output;
 				}
 			case EAST:
 				switch (furnaceFace) {
@@ -220,28 +239,31 @@ public class TileEntityFusionFurnace extends TileEntitySimpleFurnace
 				case SOUTH:
 					return slots_input2;
 				case WEST:
+                    return slots_fuel;
 				case EAST:
-					return slotsSides;
+                    return slots_output;
 				}
 			case NORTH:
 				switch (furnaceFace) {
-				case NORTH:
-				case SOUTH:
-					return slotsSides;
 				case WEST:
 					return slots_input1;
 				case EAST:
 					return slots_input2;
+				case SOUTH:
+				    return slots_fuel;
+				case NORTH:
+				    return slots_output;
 				}
 			case SOUTH:
 				switch (furnaceFace) {
-				case NORTH:
-				case SOUTH:
-					return slotsSides;
 				case WEST:
 					return slots_input2;
 				case EAST:
 					return slots_input1;
+				case NORTH:
+                    return slots_fuel;
+                case SOUTH:
+                    return slots_output;
 				}
 		}
 		return null;
@@ -251,7 +273,39 @@ public class TileEntityFusionFurnace extends TileEntitySimpleFurnace
 	@Override
 	public boolean canExtractItem(int index, ItemStack stack, EnumFacing direction) 
 	{
-		return index != 1 || stack.getItem() == Items.BUCKET;
+		return index != NDX_FUEL_SLOT || stack.getItem() == Items.BUCKET;
 	}
 	
+	   net.minecraftforge.items.IItemHandler handlerTop = new net.minecraftforge.items.wrapper.SidedInvWrapper(this, net.minecraft.util.EnumFacing.UP);
+	    net.minecraftforge.items.IItemHandler handlerBottom = new net.minecraftforge.items.wrapper.SidedInvWrapper(this, net.minecraft.util.EnumFacing.DOWN);
+	    net.minecraftforge.items.IItemHandler handlerLeftSide = new net.minecraftforge.items.wrapper.SidedInvWrapper(this, net.minecraft.util.EnumFacing.EAST);
+        net.minecraftforge.items.IItemHandler handlerRightSide = new net.minecraftforge.items.wrapper.SidedInvWrapper(this, net.minecraft.util.EnumFacing.WEST);
+        net.minecraftforge.items.IItemHandler handlerBackSide = new net.minecraftforge.items.wrapper.SidedInvWrapper(this, net.minecraft.util.EnumFacing.SOUTH);
+        net.minecraftforge.items.IItemHandler handlerFrontSide = new net.minecraftforge.items.wrapper.SidedInvWrapper(this, net.minecraft.util.EnumFacing.NORTH);
+
+	    @SuppressWarnings("unchecked")
+	    @Override
+	    public <T> T getCapability(net.minecraftforge.common.capabilities.Capability<T> capability, net.minecraft.util.EnumFacing facing)
+	    {
+	        if (facing != null && 
+	             capability == net.minecraftforge.items.CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+	        {
+	            if (facing == EnumFacing.DOWN)
+	                return (T) handlerBottom;
+	            else if (facing == EnumFacing.UP)
+	                return (T) handlerTop;
+	            else if (facing == EnumFacing.EAST)
+	                return (T) handlerLeftSide;
+	            else if (facing == EnumFacing.WEST)
+	                return (T) handlerRightSide;
+	            else if (facing == EnumFacing.SOUTH)
+                    return (T) handlerBackSide;
+	            else if (facing == EnumFacing.NORTH)
+                    return (T) handlerFrontSide;
+	            else
+	                return null;
+	        }   
+	        return super.getCapability(capability, facing);
+	    }
+
 } // end class
