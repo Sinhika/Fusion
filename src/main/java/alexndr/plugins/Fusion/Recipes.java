8e9eb8e56de90b9bfc4795fdd9fec6cc70331ffd
Sinhika <sinhika@republicofnewhome.org>
2018-10-06 09:42:01 -0500

    added JSON recipes for steel armor

--- src/main/java/alexndr/plugins/Fusion/Recipes.java
+++ src/main/java/alexndr/plugins/Fusion/Recipes.java
@@ -8,16 +8,6 @@ public class Recipes
 	
 	public static void doRecipes()
 	{	
-//		//Armor Recipes
-//		//Steel Armor
-//		GameRegistry.addRecipe(new ShapedOreRecipe(Content.steel_helmet, true, new Object[]{
-//				"XXX", "X X", Character.valueOf('X'), "ingotSteel"}));
-//		GameRegistry.addRecipe(new ShapedOreRecipe(Content.steel_chestplate, true, new Object[]{
-//				"X X", "XXX", "XXX", Character.valueOf('X'), "ingotSteel"}));
-//		GameRegistry.addRecipe(new ShapedOreRecipe(Content.steel_leggings, true, new Object[]{
-//				"XXX", "X X", "X X", Character.valueOf('X'), "ingotSteel"}));
-//		GameRegistry.addRecipe(new ShapedOreRecipe(Content.steel_boots, true, new Object[]{
-//				"X X", "X X", Character.valueOf('X'), "ingotSteel"}));
 		
 //		if (Content.use_simpleores) {
 //			RecipesSimpleOres.doRecipes();
