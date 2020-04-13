2a6d03e2d0ac7b6baa26beb64e6af2fac8f9f5fe
Cynthia Higginbotham <cynthia.c.higginboth.ctr@navy.mil>
2020-04-12 20:20:17 -0500

    started 1.15.2 port of Fusion

---
+++ src/main/java/mod/alexndr/fusion/Fusion.java
@@ -0,0 +1,35 @@
+package mod.alexndr.fusion;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Fusion.MODID)
+public class Fusion
+{
+    // modid 
+    public static final String MODID = "fusion";
+
+    // Directly reference a log4j logger.
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public Fusion()
+    {
+        LOGGER.info("Hello from Fusion!");
+        final ModLoadingContext modLoadingContext = ModLoadingContext.get();
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+//        ModBlocks.BLOCKS.register(modEventBus);
+//        ModItems.ITEMS.register(modEventBus);
+//
+//        // Register Configs
+//        modLoadingContext.registerConfig(ModConfig.Type.COMMON, ConfigHolder.SERVER_SPEC);
+
+    } // end ctor()
+
+
+} // end class
