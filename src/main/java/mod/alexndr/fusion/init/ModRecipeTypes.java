c916590ac11cd68c624253b327da69e8cf228960
Cynthia Higginbotham <cynthia.c.higginboth.ctr@navy.mil>
2020-04-23 14:39:17 -0500

    skeleton of custom recipe done, registration of ModRecipeTypes added

---
+++ src/main/java/mod/alexndr/fusion/init/ModRecipeTypes.java
@@ -0,0 +1,24 @@
+package mod.alexndr.fusion.init;
+
+import mod.alexndr.fusion.recipe.FusionRecipe;
+import mod.alexndr.fusion.recipe.IFusionRecipe;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.item.crafting.IRecipeType;
+import net.minecraft.util.registry.Registry;
+
+public class ModRecipeTypes
+{
+    public static final IRecipeType<IFusionRecipe> FUSION_TYPE = new RecipeType<>();
+    public static final IRecipeSerializer<FusionRecipe> FUSION_SERIALIZER = 
+            new FusionRecipe.FusionRecipeSerializer();
+    
+    private static class RecipeType<T extends IRecipe<?>> implements IRecipeType<T>
+    {
+        @Override
+        public String toString()
+        {
+            return Registry.RECIPE_TYPE.getKey(this).toString();
+        }
+    } // end class RecipeType
+} // end class ModRecipeTypes
---
+++ src/main/java/mod/alexndr/fusion/recipe/FusionRecipe.java
@@ -0,0 +1,146 @@
+package mod.alexndr.fusion.recipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import mod.alexndr.fusion.init.ModRecipeTypes;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.JSONUtils;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.items.wrapper.RecipeWrapper;
+import net.minecraftforge.registries.ForgeRegistryEntry;
+
+public class FusionRecipe implements IFusionRecipe
+{
+    private final ResourceLocation id;
+    private final ItemStack output;
+    private final NonNullList<Ingredient> inputs;
+    private final Ingredient catalyst;
+    private final int cook_time;
+    private final float experience;
+    
+    
+    public FusionRecipe(ResourceLocation id, ItemStack output,  int cook_time, float experience,
+                        Ingredient catalyst, Ingredient... inputs)
+    {
+        this.id = id;
+        this.output = output;
+        this.inputs = NonNullList.from(null, inputs);
+        this.catalyst = catalyst;
+        this.cook_time = cook_time;
+        this.experience = experience;
+    }
+
+    @Override
+    public boolean matches(RecipeWrapper inv, World worldIn)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public ItemStack getCraftingResult(RecipeWrapper inv)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ItemStack getRecipeOutput()
+    {
+        return this.output;
+    }
+    
+    public Ingredient getCatalyst()
+    {
+        return this.catalyst;
+    } // end class FusionRecipe
+
+    @Override
+    public ResourceLocation getId()
+    {
+        return this.id;
+    }
+
+    public int getCookTime()
+    {
+        return this.cook_time;
+    }
+    
+    public float getExperience()
+    {
+        return this.experience;
+    }
+    
+    @Override
+    public IRecipeSerializer<?> getSerializer()
+    {
+        return ModRecipeTypes.FUSION_SERIALIZER;
+    }
+
+    
+    public static class FusionRecipeSerializer extends ForgeRegistryEntry<IRecipeSerializer<?>>
+    implements IRecipeSerializer<FusionRecipe>
+    {
+
+        @Override
+        public FusionRecipe read(ResourceLocation recipeId, JsonObject json)
+        {
+            ItemStack output = CraftingHelper.getItemStack(JSONUtils.getJsonObject(json, "output"), 
+                                                           true);
+            JsonArray ingrs = JSONUtils.getJsonArray(json, "inputs");
+            List<Ingredient> inputs = new ArrayList<>();
+            for (JsonElement e : ingrs) {
+                inputs.add(Ingredient.deserialize(e));
+            }
+            Ingredient catalyst = Ingredient.fromStacks(CraftingHelper.getItemStack(
+                                                    JSONUtils.getJsonObject(json, "catalyst"),true));
+            
+            int cook_time = JSONUtils.getInt(json, "cookingtime");
+            float experience = JSONUtils.getFloat(json, "experience");
+            
+            return new FusionRecipe(recipeId, output, cook_time, experience, catalyst,
+                                     inputs.toArray(new Ingredient[0]));
+        } // end read(json)
+
+        @Override
+        public FusionRecipe read(ResourceLocation recipeId, PacketBuffer buf)
+        {
+            Ingredient[] inputs = new Ingredient[buf.readVarInt()];
+            for (int ii = 0; ii < inputs.length; ii++) {
+                inputs[ii] = Ingredient.read(buf);
+            }
+            ItemStack output = buf.readItemStack();
+            Ingredient catalyst = Ingredient.read(buf);
+            int cook_time = buf.readVarInt();
+            float exp = buf.readFloat();
+            
+            return new FusionRecipe(recipeId, output, cook_time, exp, catalyst, inputs);
+        } // end read(packet)
+
+        @Override
+        public void write(PacketBuffer buf, FusionRecipe recipe)
+        {
+            buf.writeVarInt(recipe.getIngredients().size());
+            for (Ingredient input : recipe.getIngredients()) {
+                input.write(buf);
+            }
+            buf.writeItemStack(recipe.getRecipeOutput(), false);
+            recipe.getCatalyst().write(buf);
+            buf.writeVarInt(recipe.getCookTime());
+            buf.writeFloat(recipe.getExperience());
+        } // end write(packet)
+
+    } // end class FusionRecipeSerializer
+
+} // end class FusionRecipe
