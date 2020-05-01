a0f279393a745713fa628b815253a0f7c21a3384
Cynthia Higginbotham <cynthia.c.higginboth.ctr@navy.mil>
2020-05-01 13:08:07 -0500

    added conditionals for all 4 alloy fusion recipes

--- src/main/java/mod/alexndr/fusion/config/FusionConfig.java
+++ src/main/java/mod/alexndr/fusion/config/FusionConfig.java
@@ -1,6 +1,31 @@
 package mod.alexndr.fusion.config;
 
-public class FusionConfig
+import java.util.HashMap;
+import java.util.Map;
+
+import mod.alexndr.simpleores.api.config.ISimpleConfig;
+
+public class FusionConfig implements ISimpleConfig
 {
+    private static Map<String, Boolean> flags = new HashMap<>();
+    public static FusionConfig INSTANCE = new FusionConfig();
+    
+    @Override
+    public void clear() {
+        flags.clear();
+    }
+
+    @Override
+    public boolean getFlag(String arg0)
+    {
+        Boolean obj = flags.get(arg0);
+        return obj != null && obj;
+    }
+
+    @Override
+    public void putFlag(String arg0, boolean arg1)
+    {
+        flags.put(arg0, arg1);
+    }
 
 } // end class
