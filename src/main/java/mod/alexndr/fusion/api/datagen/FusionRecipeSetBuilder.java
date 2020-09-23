361686be7e3f42afc0fa1347dea6d6420cfd2213
Sinhika <sinhika@republicofnewhome.org>
2020-09-23 12:04:49 -0500

    added functions for building recycling recipes to FusionRecipeSetBuilder
    (cherry picked from commit 08f538d00cba23bce662000bb93fa3ff6baf9451)

--- src/main/java/mod/alexndr/fusion/api/datagen/FusionRecipeSetBuilder.java
+++ src/main/java/mod/alexndr/fusion/api/datagen/FusionRecipeSetBuilder.java
@@ -21,9 +21,24 @@ public class FusionRecipeSetBuilder extends RecipeSetBuilder
         super(modid);
     }
 
+    /**
+     * Create the fusion_furnace alloy recipes for nuggets, medium chunks, and large chunks for a given pair of
+     * ingredients and set of catalysts.
+     * 
+     * @param consumer passed in from RecipeProvider to builder() call.
+     * @param primaryInputs a list of two ingredients that will be alloyed.
+     * @param catalysts 3-item array of catalysts, in order [nugget, medium_chunk, large_chunk]; null for non-existent 
+     *  recipes.
+     * @param nugget nugget item to be created by fusion alloying recipe.
+     * @param medium_chunk medium_chunk item to be created by fusion alloying recipe.
+     * @param large_chunk large_chunk item to be created by fusion alloying recipe.
+     * @param experience experience for creating nugget item. Will be multiplied x2 for medium, x4 for large.
+     * @param cooktime standard fusion furnace cooking time, usually 600.
+     * @param condition Usually a flag condition, could be more complex.
+     */
     public void buildBasicAlloyRecipes(Consumer<IFinishedRecipe> consumer, List<Ingredient> primaryInputs,
-                                       Ingredient[] catalysts, IItemProvider nugget, IItemProvider medium_chunk,
-                                       IItemProvider large_chunk, float experience, int cooktime, ICondition condition)
+            Ingredient[] catalysts, IItemProvider nugget, IItemProvider medium_chunk, IItemProvider large_chunk,
+            float experience, int cooktime, ICondition condition)
     {
         ResourceLocation nugget_name = (nugget != null) 
                 ? AbstractFusionRecipeProvider.id(modid, nugget.asItem().toString()) 
@@ -87,5 +102,42 @@ public class FusionRecipeSetBuilder extends RecipeSetBuilder
         } // end-else condition exists
     } // end buildBasicAlloyRecipes()
     
+    /**
+     * Create the fusion_furnace recipes for fusion recycling of the list of ingredients.
+     * 
+     * @param consumer passed in from RecipeProvider to builder() call.
+     * @param ingredients all the possible inputs that will produce a single output_item.
+     * @param ingrs_doubleoutput all the possible inputs that will produce 2 output items.
+     * @param input2 2nd input ingredient (often minecraft:gravel)
+     * @param catalyst catalyst ingredient.
+     * @param output_item output from recycling.
+     * @param experience experience
+     * @param cooktime cooking time; usually 600.
+     * @param condition must be here; a flag or mod_loaded or some combination.
+     * @param name simple name of recipe.
+     */
+    public void buildFusionRecyclingRecipes(Consumer<IFinishedRecipe> consumer, Ingredient ingredients,
+            Ingredient ingrs_doubleoutput, Ingredient input2, Ingredient catalyst, IItemProvider output_item, float experience,
+            int cooktime, ICondition condition, String name)
+    {
+        ResourceLocation recipe1 = AbstractFusionRecipeProvider.id(modid, name + '1');
+        ConditionalRecipe.builder()
+            .addCondition(condition)
+            .addRecipe(new AbstractFusionRecipeProvider.FinishedRecipe(recipe1,
+                            new ItemStack(output_item.asItem()), cooktime, experience, 
+                            catalyst, ingredients, input2))
+            .build(consumer, recipe1); 
+        
+        if (ingrs_doubleoutput != null)
+        {
+            ResourceLocation recipe2 = AbstractFusionRecipeProvider.id(modid, name + '2');
+            ConditionalRecipe.builder()
+                .addCondition(condition)
+                .addRecipe(new AbstractFusionRecipeProvider.FinishedRecipe(recipe1,
+                                new ItemStack(output_item.asItem(), 2), cooktime, experience * 2.0F, 
+                                catalyst, ingrs_doubleoutput, input2))
+                .build(consumer, recipe2);
+        }
+    } // end buildFusionRecyclingRecipes
     
 } // end class
