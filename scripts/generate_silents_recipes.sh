609d1cbb4905dcb4ffdb11a0e641eed9404142c5
Sinhika <sinhika@republicofnewhome.org>
2020-12-26 10:58:33 -0600

    added script to create silents_mechanisms alloy smelter and crusher recipes; added said generated recipes

--- scripts/generate_silents_recipes.sh
+++ scripts/generate_silents_recipes.sh
@@ -2,16 +2,18 @@
 
 # scripts are from mod_utils project on my GitHub.
 
+ID='fusion'
+NAME='Fusion'
 TOPDIR=`pwd`
 PROJNAME=`basename $TOPDIR`
-if [ $PROJNAME != 'Fusion' ]; then
+if [ $PROJNAME != $NAME ]; then
     echo "Running in wrong directory!"
     exit 1
 fi
 
 
 # recipes
-TARGETDIR=${TOPDIR}/src/main/resources/data/fusion/recipes
+TARGETDIR=${TOPDIR}/src/main/resources/data/${ID}/recipes
 if [ ! -d $TARGETDIR ]; then
     mkdir -p $TARGETDIR
 fi
@@ -20,23 +22,53 @@ cd $TARGETDIR
 # Silent's Mechanisms recipes
 
 # crusher
-make_silents_recipes.py --type=crusher --ingredient='fusion:bronze_ingot' \
-    --result='fusion:bronze_dust' bronze_dust_from_ingot
-make_silents_recipes.py --type=crusher --ingredient='fusion:steel_ingot' \
-    --result='fusion:steel_dust' steel_dust_from_ingot
-make_silents_recipes.py --type=crusher --ingredient='fusion:sinisite_ingot' \
-    --result='fusion:sinisite_dust' sinisite_dust_from_ingot
-make_silents_recipes.py --type=crusher --ingredient='fusion:thyrium_ingot' \
-    --result='fusion:thyrium_dust' thyrium_dust_from_ingot
+make_silents_recipes.py --type=crusher --ingredient="${ID}:bronze_ingot" \
+    --result="${ID}:bronze_dust" bronze_dust_from_ingot
+make_silents_recipes.py --type=crusher --ingredient="${ID}:steel_ingot" \
+    --result="${ID}:steel_dust" steel_dust_from_ingot
+make_silents_recipes.py --type=crusher --ingredient="${ID}:sinisite_ingot" \
+    --result="${ID}:sinisite_dust" sinisite_dust_from_ingot
+make_silents_recipes.py --type=crusher --ingredient="${ID}:thyrium_ingot" \
+    --result="${ID}:thyrium_dust" thyrium_dust_from_ingot
 
 # alloy smelter
-make_silents_recipes.py --type=alloy_smelter --ingredient '#forge:dusts/copper' \
-    '#forge:dusts/tin' 'minecraft:bone_meal' --result 'fusion:bronze_nugget' \
+make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/copper" \
+    "#forge:dusts/tin" "minecraft:bone_meal" --result "${ID}:bronze_nugget" \
     -- bronze_nugget_from_dust
-make_silents_recipes.py --type=alloy_smelter --ingredient '#forge:dusts/copper' \
-   '#forge:dusts/tin' 'minecraft:bone_meal' --result 'fusion:medium_bronze_chunk' \
+make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/copper" \
+   "#forge:dusts/tin" "minecraft:gunpowder" --result "${ID}:medium_bronze_chunk" \
    -- medium_bronze_chunk_from_dust
-make_silents_recipes.py --type=alloy_smelter --ingredient '#forge:dusts/copper' \
-   '#forge:dusts/tin' 'minecraft:redstone' --result 'fusion:large_bronze_chunk' \
+make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/copper" \
+   "#forge:dusts/tin" "minecraft:redstone" --result "${ID}:large_bronze_chunk" \
    -- large_bronze_chunk_from_dust
+
+make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/iron" \
+    "#minecraft:coals" "#minecraft:coals" --result "${ID}:steel_nugget" \
+    -- steel_nugget_from_dust
+make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/iron" \
+   "#minecraft:coals" "minecraft:gunpowder" --result "${ID}:medium_steel_chunk" \
+   -- medium_steel_chunk_from_dust
+make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/iron" \
+   "#minecraft:coals" "minecraft:redstone" --result "${ID}:large_steel_chunk" \
+   -- large_bronze_chunk_from_dust
+
+make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/mythril" \
+    "#forge:dusts/adamantium" "minecraft:redstone" --result "${ID}:thyrium_nugget" \
+    -- thyrium_nugget_from_dust
+make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/mythril" \
+   "#forge:dusts/adamantium" "minecraft:lapis_lazuli" --result "${ID}:medium_thyrium_chunk" \
+   -- medium_thyrium_chunk_from_dust
+make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/mythril" \
+   "#forge:dusts/adamantium" "minecraft:glowstone_dust" --result "${ID}:large_thyrium_chunk" \
+   -- large_thyrium_chunk_from_dust
+
+make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/mythril" \
+    "#forge:gems/onyx" "minecraft:glowstone_dust" --result "${ID}:sinisite_nugget" \
+    -- sinisite_nugget_from_dust
+make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/mythril" \
+   "#forge:gems/onyx" "minecraft:blaze_powder" --result "${ID}:medium_sinisite_chunk" \
+   -- medium_sinisite_chunk_from_dust
+make_silents_recipes.py --type=alloy_smelter --ingredient "#forge:dusts/mythril" \
+   "#forge:gems/onyx" "minecraft:ghast_tear" --result "${ID}:large_sinisite_chunk" \
+   -- large_sinisite_chunk_from_dust
 
