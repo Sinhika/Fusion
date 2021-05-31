b47281a071c5b8987e2cedd7d162944660076007
Sinhika <sinhika@republicofnewhome.org>
2021-05-31 16:03:07 -0500

    refactored FusionConfig

--- src/main/java/mod/alexndr/fusion/config/FusionConfig.java
+++ src/main/java/mod/alexndr/fusion/config/FusionConfig.java
@@ -1,32 +1,9 @@
 package mod.alexndr.fusion.config;
 
-import java.util.HashMap;
-import java.util.Map;
+import mod.alexndr.simplecorelib.config.SimpleConfig;
 
-import mod.alexndr.simplecorelib.config.ISimpleConfig;
-
-public class FusionConfig implements ISimpleConfig
+public class FusionConfig extends SimpleConfig
 {
-    private static Map<String, Boolean> flags = new HashMap<>();
     public static FusionConfig INSTANCE = new FusionConfig();
     public static boolean addChestLoot;
-    
-    @Override
-    public void clear() {
-        flags.clear();
-    }
-
-    @Override
-    public boolean getFlag(String arg0)
-    {
-        Boolean obj = flags.get(arg0);
-        return obj != null && obj;
-    }
-
-    @Override
-    public void putFlag(String arg0, boolean arg1)
-    {
-        flags.put(arg0, arg1);
-    }
-
 } // end class
