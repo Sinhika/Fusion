d48f95c6fd1f69f9125b13fb847bf2c5dddf25cb
Sinhika <sinhika@republicofnewhome.org>
2022-05-02 12:51:56 -0500

    fixing fuel for fusion furnaces--we're doing it wrong

--- src/main/java/mod/alexndr/fusion/client/jei/FusionFuelCategory.java
+++ src/main/java/mod/alexndr/fusion/client/jei/FusionFuelCategory.java
@@ -1,10 +1,18 @@
 package mod.alexndr.fusion.client.jei;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
 import mezz.jei.api.gui.drawable.IDrawableStatic;
 import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
 import mod.alexndr.fusion.Fusion;
 import mod.alexndr.fusion.client.ClientModEventSubscriber;
+import mod.alexndr.netherrocks.client.jei.NetherFurnaceFuelRecipe;
 import mod.alexndr.simplecorelib.api.client.jei.AlternateFuelRecipe;
 import mod.alexndr.simplecorelib.api.client.jei.VeryAbstractFurnaceVariantCategory;
 import net.minecraft.client.Minecraft;
@@ -13,7 +21,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 
-public class FusionFuelCategory extends VeryAbstractFurnaceVariantCategory<AlternateFuelRecipe>
+public class FusionFuelCategory extends VeryAbstractFurnaceVariantCategory<FusionFurnaceFuelRecipe>
 {
 	public static final ResourceLocation UID = new ResourceLocation(Fusion.MODID, "fusion_furnace_fuel");
 	
@@ -41,16 +49,24 @@ public class FusionFuelCategory extends VeryAbstractFurnaceVariantCategory<Alter
 		localizedName = new TranslatableComponent("gui.jei.category.fusion_furnace_fuel");
 	}
 
+	
 	@Override
+    public RecipeType<FusionFurnaceFuelRecipe> getRecipeType()
+    {
+        return JEIFusionPlugin.FUSION_FUEL_RECIPE_TYPE;
+    }
+
+
+    @Override
 	public ResourceLocation getUid()
 	{
 		return UID;
 	}
 
 	@Override
-	public Class<? extends AlternateFuelRecipe> getRecipeClass()
+	public Class<? extends FusionFurnaceFuelRecipe> getRecipeClass()
 	{
-		return AlternateFuelRecipe.class;
+		return FusionFurnaceFuelRecipe.class;
 	}
 
 	@Override
@@ -70,5 +86,22 @@ public class FusionFuelCategory extends VeryAbstractFurnaceVariantCategory<Alter
 	{
 		return flameTransparentBackground;
 	}
+
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, FusionFurnaceFuelRecipe recipe, IFocusGroup focuses)
+    {
+        builder.addSlot(RecipeIngredientRole.INPUT, 1, 17).addItemStacks(recipe.getInputs());
+    }
+
+    @Override
+    public void draw(FusionFurnaceFuelRecipe recipe, PoseStack poseStack, double mouseX, double mouseY) 
+    {
+        IDrawableAnimated flame = this.getFlame();
+        flame.draw(poseStack, 1, 0);
+        Minecraft minecraft = Minecraft.getInstance();
+        this.smeltCountText = createSmeltCountText(recipe.getBurnTime());
+        minecraft.font.draw(poseStack, smeltCountText, 24, 13, 0xFF808080);
+    }
 
 } // end class
--- src/main/java/mod/alexndr/fusion/client/jei/FusionFurnaceFuelRecipe.java
+++ src/main/java/mod/alexndr/fusion/client/jei/FusionFurnaceFuelRecipe.java
@@ -2,6 +2,7 @@ package mod.alexndr.fusion.client.jei;
 
 import java.util.Collection;
 
+import mod.alexndr.fusion.api.content.AbstractAlloyFurnaceTileEntity;
 import mod.alexndr.simplecorelib.api.client.jei.AlternateFuelRecipe;
 import net.minecraft.world.item.ItemStack;
 
@@ -11,7 +12,7 @@ public class FusionFurnaceFuelRecipe extends AlternateFuelRecipe
     public FusionFurnaceFuelRecipe(Collection<ItemStack> input, int burnTime)
     {
         super(input, burnTime);
-        BURN_TIME_STANDARD = 100;
+        BURN_TIME_STANDARD = (int) Math.ceil(200.0F * AbstractAlloyFurnaceTileEntity.BURN_TIME_MODIFIER);
     }
 
 } // end class
